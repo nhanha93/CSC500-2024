9e0301e86db5f5abdd77b2687ef3424e9d836d69
nhanha93 <67775540+nhanha93@users.noreply.github.com>
2024-07-18 17:18:15 -0500

    Add files via upload

---
+++ Module 1/Part1.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+add = num1 + num2
+subtract = num1 - num2
+print("Addition is: ", add)
+print("Subtraction is: ",subtract)
---
+++ Module 1/Part2.py
@@ -0,0 +1,6 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+multiply = num1 * num2
+divide = num1 / num2
+print("Addition is: ", multiply)
+print("Subtraction is: ",divide)
